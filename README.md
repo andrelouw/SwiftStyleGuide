fb1b2d4f68070d36b7b46fc6e07ef09c1e3a952b
Andre Louw <andrelouw@hey.com>
2023-03-11 15:20:05 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftStyleGuide
+
+A description of this package.
---
+++ Sources/SwiftStyleGuide/SwiftStyleGuide.swift
@@ -0,0 +1,6 @@
+public struct SwiftStyleGuide {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
