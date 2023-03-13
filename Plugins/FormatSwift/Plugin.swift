044aa48c5efd3b2655c6f8f91811b940492f444d
Andre Louw <andrelouw@hey.com>
2023-03-13 09:08:45 +0100

    Add input paths helper to get all paths to lint

--- Plugins/FormatSwift/Plugin.swift
+++ Plugins/FormatSwift/Plugin.swift
@@ -5,25 +5,27 @@ import PackagePlugin
 struct FormatSwift: CommandPlugin {
   func performCommand(
     context: PluginContext,
-    arguments _: [String]
+    arguments: [String]
   ) async throws {
     // Path options:
     // - Add specific paths using `--paths`
     // - Add specific targets using `--targets`
-    // - defaults to running on whole directory
-    let workingDirectory = context.package.directory
-    let swiftLintConfigFilePath = workingDirectory.appending("swiftlint.yml").string
-    let swiftFormatConfigFilePath = workingDirectory.appending("swiftformat").string
+    // - defaults to running on whole directory (by using input paths helper)
+    let inputPaths = try inputPaths(for: context.package)
+
+//    let workingDirectory = context.package.directory
+//    let swiftLintConfigFilePath = workingDirectory.appending("swiftlint.yml").string
+//    let swiftFormatConfigFilePath = workingDirectory.appending("swiftformat").string
 
     let launchPath = try context.tool(named: "SwiftStyleGuideTool").path.string
 
-    var arguments = [workingDirectory.string]
+    var toolArguments = inputPaths
 
-    arguments += [
+    toolArguments += [
       "--swift-format-path", "/opt/homebrew/bin/swiftformat",
-      "--swift-format-config", swiftFormatConfigFilePath,
+//      "--swift-format-config", swiftFormatConfigFilePath,
       "--swift-lint-path", "/opt/homebrew/bin/swiftlint",
-      "--swift-lint-config", swiftLintConfigFilePath,
+//      "--swift-lint-config", swiftLintConfigFilePath,
       "--swift-format-cache-path",
       context.pluginWorkDirectory.string + "/swiftformat.cache",
       "--swift-lint-cache-path",
@@ -33,7 +35,7 @@ struct FormatSwift: CommandPlugin {
 
     let process = Process()
     process.launchPath = launchPath
-    process.arguments = arguments
+    process.arguments = toolArguments
     try process.run()
 
     process.waitUntilExit()
@@ -44,6 +46,27 @@ struct FormatSwift: CommandPlugin {
     default:
       throw CommandError.unknownError(exitCode: process.terminationStatus)
     }
+  }
+
+  /// Retrieves the list of paths that should be formatted / linted
+  ///
+  /// By default this tool runs on all subdirectories of the package's root directory,
+  /// plus any Swift files directly contained in the root directory. This is a
+  /// workaround for two interesting issues:
+  ///  - If we lint `content.package.directory`, then SwiftLint lints the `.build` subdirectory,
+  ///    which includes checkouts for any SPM dependencies, even if we add `.build` to the
+  ///    `excluded` configuration in our `swiftlint.yml`.
+  ///  - We could lint `context.package.targets.map { $0.directory }`, but that excludes
+  ///    plugin targets, which include Swift code that we want to lint.
+  private func inputPaths(for package: Package) throws -> [String] {
+    let packageDirectoryContents = try FileManager.default.contentsOfDirectory(
+      at: URL(fileURLWithPath: package.directory.string),
+      includingPropertiesForKeys: nil,
+      options: [.skipsHiddenFiles])
+
+    let subdirectories = packageDirectoryContents.filter { $0.hasDirectoryPath }
+    let rootSwiftFiles = packageDirectoryContents.filter { $0.pathExtension.hasSuffix("swift") }
+    return (subdirectories + rootSwiftFiles).map { $0.path }
   }
 }
 
