40fc3613717db1cfc9abfd60935feab9f2c4d10d
Andre Louw <andrelouw@hey.com>
2023-03-13 08:49:43 +0100

    Add package swiftlint config file

--- Plugins/FormatSwift/Plugin.swift
+++ Plugins/FormatSwift/Plugin.swift
@@ -11,15 +11,17 @@ struct FormatSwift: CommandPlugin {
     // - Add specific paths using `--paths`
     // - Add specific targets using `--targets`
     // - defaults to running on whole directory
-    let workingDirectory = context.package.directory.string
+    let workingDirectory = context.package.directory
+    let swiftLintConfigFilePath = workingDirectory.appending("swiftlint.yml").string
 
     let launchPath = try context.tool(named: "SwiftStyleGuideTool").path.string
 
-    var arguments = [workingDirectory]
+    var arguments = [workingDirectory.string]
 
     arguments += [
       "--swift-format-path", "/opt/homebrew/bin/swiftformat",
       "--swift-lint-path", "/opt/homebrew/bin/swiftlint",
+      "--swift-lint-config", swiftLintConfigFilePath,
       "--swift-format-cache-path",
       context.pluginWorkDirectory.string + "/swiftformat.cache",
       "--swift-lint-cache-path",
