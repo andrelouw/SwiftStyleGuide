c865682cf993f2e3d7429d99be6067b1df706abe
Andre Louw <andrelouw@hey.com>
2023-03-13 09:26:16 +0100

    Extract build up of paths and arguments

--- Plugins/FormatSwift/Plugin.swift
+++ Plugins/FormatSwift/Plugin.swift
@@ -7,50 +7,9 @@ struct FormatSwift: CommandPlugin {
     context: PluginContext,
     arguments: [String]
   ) async throws {
-    // Path options:
-    // - Add specific paths using `--paths`
-    // - Add specific targets using `--targets`
-    // - defaults to running on whole directory (by using input paths helper)
-
-    var argumentExtractor = ArgumentExtractor(arguments)
-
-    // When ran from Xcode, the plugin command is invoked with `--target` arguments,
-    // specifying the targets selected in the plugin dialog.
-    let inputTargets = argumentExtractor.extractOption(named: "target")
-
-    // If given, lint only the paths passed to `--paths`
-    var inputPaths = argumentExtractor.extractOption(named: "paths")
-
-    if !inputTargets.isEmpty {
-      // If a set of input targets were given, lint/format the directory for each of them
-      inputPaths += try context.package.targets(named: inputTargets).map { $0.directory.string }
-    }
-
-    if inputPaths.isEmpty {
-      // Otherwise if no targets or paths listed we default to linting/formatting
-      // the entire package directory.
-      inputPaths = try self.inputPaths(for: context.package)
-    }
-
-//    let workingDirectory = context.package.directory
-//    let swiftLintConfigFilePath = workingDirectory.appending("swiftlint.yml").string
-//    let swiftFormatConfigFilePath = workingDirectory.appending("swiftformat").string
-
+    let paths = try determinePaths(from: arguments, context: context)
+    let toolArguments = toolArguments(paths: paths, context: context)
     let launchPath = try context.tool(named: "SwiftStyleGuideTool").path.string
-
-    var toolArguments = inputPaths
-
-    toolArguments += [
-      "--swift-format-path", "/opt/homebrew/bin/swiftformat",
-//      "--swift-format-config", swiftFormatConfigFilePath,
-      "--swift-lint-path", "/opt/homebrew/bin/swiftlint",
-//      "--swift-lint-config", swiftLintConfigFilePath,
-      "--swift-format-cache-path",
-      context.pluginWorkDirectory.string + "/swiftformat.cache",
-      "--swift-lint-cache-path",
-      context.pluginWorkDirectory.string + "/swiftlint.cache",
-      "--log", "--swift-lint-only-lint", "--swift-format-only-lint"
-    ]
 
     let process = Process()
     process.launchPath = launchPath
@@ -67,6 +26,51 @@ struct FormatSwift: CommandPlugin {
     }
   }
 
+  private func toolArguments(paths: [String], context: PluginContext) -> [String] {
+    var arguments = paths
+
+    // To get config files in package root
+    //    let workingDirectory = context.package.directory
+    //    let swiftLintConfigFilePath = workingDirectory.appending("swiftlint.yml").string
+    //    let swiftFormatConfigFilePath = workingDirectory.appending("swiftformat").string
+
+    arguments += [
+      "--swift-format-path", "/opt/homebrew/bin/swiftformat",
+      //      "--swift-format-config", swiftFormatConfigFilePath,
+      "--swift-lint-path", "/opt/homebrew/bin/swiftlint",
+      //      "--swift-lint-config", swiftLintConfigFilePath,
+      "--swift-format-cache-path",
+      context.pluginWorkDirectory.string + "/swiftformat.cache",
+      "--swift-lint-cache-path",
+      context.pluginWorkDirectory.string + "/swiftlint.cache",
+      "--log", "--swift-lint-only-lint", "--swift-format-only-lint"
+    ]
+
+    return arguments
+  }
+
+  private func determinePaths(from arguments: [String], context: PluginContext) throws -> [String] {
+    var argumentExtractor = ArgumentExtractor(arguments)
+
+    // When ran from Xcode, the plugin command is invoked with `--target` arguments,
+    // specifying the targets selected in the plugin dialog.
+    let inputTargets = argumentExtractor.extractOption(named: "target")
+
+    // If given, lint only the paths passed to `--paths`
+    var paths = argumentExtractor.extractOption(named: "paths")
+
+    if !inputTargets.isEmpty {
+      // If a set of input targets were given, lint/format the directory for each of them
+      paths += try context.package.targets(named: inputTargets).map { $0.directory.string }
+    } else if paths.isEmpty {
+      // Otherwise if no targets or paths listed we default to linting/formatting
+      // the entire package directory.
+      paths = try allPaths(for: context.package)
+    }
+
+    return paths
+  }
+
   /// Retrieves the list of paths that should be formatted / linted
   ///
   /// By default this tool runs on all subdirectories of the package's root directory,
@@ -77,7 +81,7 @@ struct FormatSwift: CommandPlugin {
   ///    `excluded` configuration in our `swiftlint.yml`.
   ///  - We could lint `context.package.targets.map { $0.directory }`, but that excludes
   ///    plugin targets, which include Swift code that we want to lint.
-  private func inputPaths(for package: Package) throws -> [String] {
+  private func allPaths(for package: Package) throws -> [String] {
     let packageDirectoryContents = try FileManager.default.contentsOfDirectory(
       at: URL(fileURLWithPath: package.directory.string),
       includingPropertiesForKeys: nil,
